d3ea025b0631f668fe6e6651a76af6d2dbacc104
ithai5 <ithai.g@gmail.com>
2024-11-10 11:24:49 +0100

    set design to match requirements

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import styles from "./components/CharacterEditor/CharacterEditor.module.css";
 
 function App() {
   return (
-    <>
-      <CharacterEditor />
-      <Footer />
-    </>
+      <>
+
+          <CharacterEditor/>
+          <Footer/>
+      </>
   );
 }
 
